5c012e0201d285b9832b15258ca3e44fb9c62e15
Sancara <santiagocaraballo@gmail.com>
2022-01-20 23:36:03 -0300

    setting alarm after finishing

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -19,6 +19,23 @@ class ViewController: UIViewController {
     var timer = Timer()
     var player: AVAudioPlayer?
     
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            player!.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
         timer.invalidate()
@@ -38,29 +55,7 @@ class ViewController: UIViewController {
             } else {
                 Timer.invalidate()
                 titleLabel.text = "DONE"
-                
-
-                func playSound() {
-                    guard let url = Bundle.main.url(forResource: "soundName", withExtension: "mp3") else { return }
-
-                    do {
-                        try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-                        try AVAudioSession.sharedInstance().setActive(true)
-
-                        /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
-                        player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
-
-                        /* iOS 10 and earlier require the following line:
-                        player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
-
-                        guard let player = player else { return }
-
-                        player.play()
-
-                    } catch let error {
-                        print(error.localizedDescription)
-                    }
-                }
+                playSound()
             }
             
         }
